45fbefe6b25099f802a7914b381d1a3e99bd085f
Mikko Partio <mikko.partio@fmi.fi>
2022-05-21 16:45:57 +0300

    Set host-bucket to empty string

--- copy-to-lake.sh
+++ copy-to-lake.sh
@@ -7,7 +7,7 @@ date=$(date +%Y%m%d)
 for filename in $*; do
   s3cmd -v \
     --host $S3_HOSTNAME \
-    --host-bucket=routines-data.$S3_HOSTNAME \
+    --host-bucket='' \
     --access_key $S3_ACCESS_KEY_ID \
     --secret_key $S3_SECRET_ACCESS_KEY \
     put $filename s3://mos-data/$TYPE/$DTG/$(basename $filename)
